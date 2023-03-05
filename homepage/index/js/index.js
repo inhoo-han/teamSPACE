f7cf2cedf07eb896a3aa3591e9a7c6fcefc75cb4
Inhoo Han <a5019999@naver.com>
2023-03-06 01:56:49 +0900

    230306 01:56

--- homepage/index/js/index.js
+++ homepage/index/js/index.js
@@ -4,27 +4,21 @@ $(function() {
         let scroll_top = $(window).scrollTop();
         console.log(scroll_top);
         let documentHeight = $(document).height();
-        console.log(documentHeight);
-
+        console.log(documentHeight); 
+        
         /* earth size */
         $('#earth').css({
             transform: 'scale(' + (scrollY + 200)/200 + ')'
         });
-
-        /* skip버튼 클릭 */
-        $('.skip').click(function() {
-            // $('html, body').animate({
-            //     scrollTop : documentHeight
-            // }, 1000);
-            $('html,body').scrollTop(documentHeight);
-        });
     });
-});
+ 
+    /* skip버튼 클릭 */
+    $('.skip').click(function() {
+        $('html, body').animate({
+            scrollTop : $(document).height()
+        }, 1000);
+        // $('html,body').scrollTop( $(document).height());
+    });
 
-// window.onload = function() {
-//     const earth = document.getElementById('earth');
-//     window.addEventListener('scroll', function() {
-//         earth.style.transform = 'scale(' + 0.5 + window.pageYOffset/500 + ')';
-//         // bg.style.width = 200 + window.pageYOffset/120 + '%';
-//     });
-// }
+    // $('.cele').imgExpand(); 
+});
