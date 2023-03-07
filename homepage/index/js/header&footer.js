e02417be34fc239f389e8fa772a098667d791626
inhoo-han <a5019999@naver.com>
2023-03-08 08:42:51 +0900

    230308

--- homepage/index/js/header&footer.js
+++ homepage/index/js/header&footer.js
@@ -1,6 +1,7 @@
 $(function() {
     $('.tMenu>.menu').hover(function() {
         $(this).find('.subMenu').stop().slideDown(1000);
+        $(this).find('a').css(color = '#fff');
     }, function() {
         $(this).find('.subMenu').stop().slideUp(1000);
     });
