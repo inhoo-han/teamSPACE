538b934f3fa98ecf12d4749493ed8a84dcaf2bc8
Inhoo Han <a5019999@naver.com>
2023-03-08 02:09:06 +0900

    230308 02:09

--- homepage/index/js/header&footer.js
+++ homepage/index/js/header&footer.js
@@ -7,4 +7,73 @@ $(function() {
     $('.home').click(function() {
         window.location = 'index.html'
     });
+
+    let loginSW = false;
+    $('#login_area .srch_btn').click(function() {
+        // $('#header_search').slideToggle("fast");
+        loginSW = !loginSW;
+        if(loginSW) {
+            $('#header_search').animate({
+                top: '100px'
+            });
+        }else {
+            $('#header_search').animate({
+                top: '0px'
+            });
+        }
+    });
+
+    /* * * * * hover시 color변경 영역 * * * * */
+    /* 서브메뉴 hover */
+    $('.smenu>a').hover(function() {
+        $(this).css({
+            color: '#fff'
+        });
+    }, function() {
+        $(this).css({
+            color: '#ccc'
+        });
+    });
+    /* 탑메뉴 hover */
+    $('.menu').hover(function() {
+        $(this).children('a').css({
+            color: '#fff'
+        });
+    }, function() {
+        $(this).children('a').css({
+            color: '#ccc'
+        });
+    });
+    /* 로그인 영역 hover */
+    $('#login_area li').hover(function() {
+        $(this).find('a').css({
+            color: '#fff'
+        });
+    }, function() {
+        $(this).find('a').css({
+            color: '#ccc'
+        });
+    });
+    /* footer link영역 hover */
+    $('#footer_link li').hover(function() {
+        $(this).find('a').css({
+            color: '#fff'
+        });
+    }, function() {
+        $(this).find('a').css({
+            color: '#ccc'
+        });
+    });
+    /* 로그인 영역 돋보기 hover */
+    $('#login_area .srch_btn').hover(function() {
+        $('.srch_img').css({
+            filter: 'invert(100%)'
+        });
+    }, function() {
+        $('.srch_img').css({
+            filter: 'invert(80%)'
+        });
+    });
+
+
 });
