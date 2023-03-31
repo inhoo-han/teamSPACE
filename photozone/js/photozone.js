2c03e4b9000a6130a156b1a16cdf15892741c7ed
Inhoo Han <a5019999@naver.com>
2023-03-31 23:28:14 +0900

    포토존 1차완성

--- photozone/js/photozone.js
+++ photozone/js/photozone.js
@@ -1,12 +1,50 @@
 $(function() {
+    /* 배경사진 선택 시 변경 */
     $('#choice_zone').find('img').click(function() {
+        $('.photo_choice_notice').css('display','none');
         let sampleSRC = $(this).attr('src');
         // alert(sampleSRC);
-        $('#photo_onload').find('img').attr('src', sampleSRC);
+        $('#bg_img_click').attr('src', sampleSRC);
     });
-    $('.user_photo_div').resizable();        
+    /* user_photo 리사이즈, 드래그 */
+    $('.user_photo_div').resizable({
+        minWidth: 100,
+        maxWidth: 500
+    });            
     $('.user_photo_div').draggable();   
 
-
-
+    /* 사진 업로드 후 user_photo적용 영역 */
+    $(":input[name='p_upload']").change(function() {
+		if( $(":input[name='p_upload']").val() == '' ) {
+			$('#user_photo').attr('src' , 'images/mycat.png');  
+		}
+		readURL(this);
+	});
 });
+
+/* 사진 업로드 함수 */
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            $('#user_photo').attr('src', e.target.result); 
+        }
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+/* 이미지를 캡처하여 이미지 파일로 리턴함 */
+function downImg(){
+    html2canvas($("#photo_onload")[0]).then(function(canvas){
+        var myImage = canvas.toDataURL();
+        downloadURI(myImage, "una_myimg.png") 
+    });
+}
+/* 캡처된 파일을 이미지 파일로 내보냄 */
+function downloadURI(uri, name){
+    var link = document.createElement("a")
+    link.download = name;
+    link.href = uri;
+    document.body.appendChild(link);
+    link.click();
+}
