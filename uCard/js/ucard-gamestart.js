540fb67f2850548c8d9702c13dde83f3b4903145
Inhoo Han <a5019999@naver.com>
2023-04-29 12:41:34 +0900

    ucard js 수정필요

--- uCard/js/ucard-gamestart.js
+++ uCard/js/ucard-gamestart.js
@@ -1,6 +1,6 @@
 //https://drcode-devblog.tistory.com/m/215
 //HashMap 객체 생성
-let JaMap = function() {
+let JqMap = function() {
     this.map = new Object();
 }
 
@@ -163,7 +163,177 @@ function makeCard(gameBoard) {
     
     let boardDiv = "";
     for(let i=0; i<gameBoard.length; i++) {
-        var rowDiv = "<div class=row>\n";
-
+        let rowDiv = "<div class=row>\n";
+        rowDiv += "<div class=rowNum><br><br><br>" + parseInt(i+1) + "</div>";
+        for(let j=0; j<gameBoard[i].length; j++){
+            let num = gameBoard[i][j] % 10 == 0 ? 10 : gameBoard[i][j];
+            let cardDiv = "<div name=card class=card id=" + gameBoard[i][j] + " ";
+            cardDiv += "style = background-color:" + clickCard(num) + ">";
+            cardDiv += "<input type=hidden value=" + gameBoard[i][j] + "/>";
+            cardDiv += "</div>\n";
+            rowDiv += cardDiv;
+        }
+        rowDiv += "</div>\n";
+        boardDiv += rowDiv;
     }
+    board.innerHTML = boardDiv;
+}
+
+//카드 클릭시 이미지 로드
+//일단 색깔로 했는데 추후 이미지로 변경
+function clickCard(num) {
+    num %= 10;
+    let color="";
+
+    switch(num) {
+        case 0: color="black"; break;
+        case 1: color="red"; break;
+        case 2: color="blue"; break;
+        case 3: color="brown"; break;
+        case 4: color="green"; break;
+        case 5: color="slateblue"; break;
+        case 6: color="lightpink"; break;
+        case 7: color="lightyellow"; break;
+        case 8: color="oragne"; break;
+        case 9: color="lightred"; break;
+    }
+    return color;
+}
+
+// 카운트 다운 프로그래스 바
+function countDown() {
+
+    //화면의 높이와 너비를 구한다.
+    var maskHeight = $(document).height();  
+    var maskWidth = $(window).width();  
+
+    //마스크의 높이와 너비를 화면 것으로 만들어 전체 화면을 채운다.
+    $('#mask').css({'width':maskWidth,'height':maskHeight});  
+
+    //애니메이션 효과
+    $('#mask').css("display", "block");      
+
+    var timeleft = 10;
+    var downloadTimer = setInterval(function(){
+        if(timeleft <= 0){
+        clearInterval(downloadTimer);
+        $(".card").css("background", "white");
+        $("#mask").css("display", "none");
+        }
+        document.getElementById("progressBar").value = 10 - timeleft;
+        timeleft -= 1;
+    }, 1000);
+}
+
+//게임 시작
+function start() {
+    cnt = 10;
+    turn = 0;
+    // $("#turn").text(orderMap.get(orders[turn]));
+    // $("#arrow" + parseInt(orders[turn]+1)).css("visibility","visible");
+    $("input[name=player]").val(0);
+
+    //2차원 배열 생성
+    let board = makeGameBoard(4,5);
+
+    //난수 발생시켜 넣을 배열값
+    let arrRandom = makeRandomNum(4*5);
+
+    //2차원 배열에 값 넣기
+    board = insertValue(board, arrRandom);
+
+    //카드 생성
+    makeCard(board);
+
+    //카운트 다운
+    countDown();
+
+    $(".card").mouseover(function(e) {
+        if(e.target.firstChild.value != clicked[0]) e.target.style.background = "skyblue";
+    });
+
+    $(".card").mouseout(function(e) {
+        if(e.clicked.length == 0) e.target.style.background = "";
+        else{
+            if(e.target.firstChild.value != clicked[0]) e.target.style.background = "";
+        }
+    });
+
+    $(".card").click(function() {
+        let number = $(this).children().eq(0).val();
+        let choice;
+
+        if(number != "") {
+            if(clicked.length == 0) {
+                clicked.push(number);
+                e.target.style.backgroundColor = clickCard(number);
+            }else {
+                let chk = false;
+                for(let i=0; i<clicked.length; i++) {
+                    if(parseInt(clicked[i]) == parseInt(number)) {
+                        chk = true;
+                        break;
+                    }
+                }
+                if(!chk) {
+                    e.target.style.backgroundColor = clickCard(number);
+                    clicked.push(number);
+                    setInterval(function() {
+                        if(clicked.length == 2) {
+                            if(parseInt(clicked[0]) % 10 == parseInt(clicked[1]) % 10){
+                                // let score = $("#p" + parseInt(orders[turn]+1)).val();
+                                // $("#p" + parseInt(orders[turn]+1)).val(++score);
+                                // scoreArr[orders[turn]]++;
+                                // cnt--;
+
+                                $("#" + clicked[0]).addClass("found");
+                                $("#" + clicked[1]).addClass("found");
+
+                                document.getElementById(clicked[0]).firstChild.value = "";
+                                document.getElementById(clicked[1]).firstChild.value = "";
+
+                                $(".found").removeClass("card");
+                                $(".found").css("background", "");
+                                $(".found").css("border", "");
+                            }else {
+                                $("#" + clicked[0]).css("background", "");
+                                $("#" + clicked[1]).css("background", "");
+                            }
+                            while(clicked.length > 0) clicked.pop();
+                            $(".card").css("background", "");
+                        }
+                        if(cnt == 0) {
+									
+
+                            var max = 0;
+                            var maxArr = new Array();
+
+                            for (var i = 0; i < scoreArr.length; i++) {
+                                max = Math.max(scoreArr[i], max);
+                            }
+
+                            for (var i = 0; i < scoreArr.length; i++) {
+                                if(scoreArr[i] == max) {
+                                    maxArr.push(i);
+                                }
+                            }
+
+                            if(maxArr.length == 1) {
+                                $("#winner").text(orderMap.get(maxArr[0]) +" 가(이) 이겼습니다");
+                            } else {
+                                var msg = "";
+                                for (var i = 0; i < maxArr.length; i++) {
+                                    msg += orderMap.get(maxArr[i]);
+                                    if(i < maxArr.length-1) msg += " 과(와) ";
+                                    else msg += " 가(이) 동점으로 무승부입니다.";
+                                }
+
+                                $("#winner").text(msg);
+                            }
+                        }
+                    }, 600);
+                }
+            }
+        }
+    });
 }
